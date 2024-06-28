fd32ddd0598043e87fc1d129be47eba1b1114dc7
Prystmartin <prystmartin@icloud.com>
2024-06-28 12:31:46 -0500

    added ec2

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ resource "aws_iam_user" "lb" {
     tag-key = "tag-value"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
